553df33c6cabd2b3c451b770f4a8cdda0eec9486
Alexander Lozovsky <lozovsky.alx@yandex.ru>
2022-10-12 12:08:48 +0200

    move logic closer to routes

--- src/routes/newIssue.tsx
+++ src/routes/newIssue.tsx
@@ -1,6 +1,14 @@
-import { FC } from 'react';
-import { useOutletContext, useNavigate, ActionFunction, redirect } from 'react-router-dom';
-import CreateIssueModal from '../components/CreateIssueModal';
+import { FC, useMemo } from 'react';
+import { createPortal } from 'react-dom';
+import {
+    useOutletContext,
+    useNavigate,
+    ActionFunction,
+    redirect,
+    Form,
+    useActionData,
+    useNavigation,
+} from 'react-router-dom';
 import graphqlClient from '../graphQL/client';
 import { CreateIssueDocument } from '../graphQL/generated-types';
 
@@ -8,7 +16,52 @@ const NewIssue: FC = () => {
     const { repositoryId } = useOutletContext() as { repositoryId: string };
     const navigate = useNavigate();
 
-    return <CreateIssueModal onClose={() => navigate(-1)} repositoryId={repositoryId} />;
+    const modalRoot = useMemo(() => document.getElementById('modal-root'), []);
+    const error = useActionData();
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+    const onClose = () => navigate(-1);
+
+    const modal = (
+        <div
+            className="fixed flex items-center justify-center w-screen h-screen top-0 left-0 bg-overlay"
+            onClick={onClose}
+        >
+            <div className="w-[600px] bg-white py-3 px-8" onClick={(e) => e.stopPropagation()}>
+                <div>
+                    <h1 className="font-bold text-2xl">Create New Issue</h1>
+                    {!!error && <p className="text-error">Cannot create issue, please try again</p>}
+                </div>
+                <Form method="post">
+                    <input
+                        className="block w-full border mt-6 p-2"
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        required
+                    />
+                    <textarea
+                        className="block w-full border mt-5 p-2 resize-none"
+                        name="description"
+                        rows={8}
+                        placeholder="Description"
+                    />
+                    <input hidden type="text" defaultValue={repositoryId} name="repositoryId" />
+
+                    <div className="text-right mt-12">
+                        <button type="button" className="text-base px-4 py-2 bg-error mr-2" onClick={onClose}>
+                            Cancel
+                        </button>
+                        <button type="submit" className="text-base px-4 py-2 bg-success" disabled={isSubmitting}>
+                            Create
+                        </button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    );
+
+    return createPortal(modal, modalRoot);
 };
 
 export const action: ActionFunction = async ({ request, params }) => {
--- src/routes/repository.tsx
+++ src/routes/repository.tsx
@@ -1,18 +1,10 @@
 import { FC } from 'react';
-import { LoaderFunction } from 'react-router-dom';
+import { ApolloQueryResult } from '@apollo/client';
+import { LoaderFunction, useLoaderData, Form, Outlet } from 'react-router-dom';
 import graphqlClient from '../graphQL/client';
 import Searchbox from '../components/Searchbox';
-import RepositoryPageContent from '../components/RepositoryPageContent';
-import { GetRepositoryDocument } from '../graphQL/generated-types';
-
-const Repository: FC = () => {
-    return (
-        <div className="w-container mx-auto pt-32">
-            <Searchbox />
-            <RepositoryPageContent />
-        </div>
-    );
-};
+import { GetRepositoryDocument, GetRepositoryQuery } from '../graphQL/generated-types';
+import { differenceInDays } from '../utils';
 
 export const loader: LoaderFunction = async ({ params }) => {
     const { userId, repositoryName } = params;
@@ -28,6 +20,68 @@ export const loader: LoaderFunction = async ({ params }) => {
     } catch (e: any) {
         return { error: e.message, loading: false };
     }
+};
+
+const Repository: FC = () => {
+    const result = useLoaderData() as ApolloQueryResult<GetRepositoryQuery>;
+
+    if (result.error) {
+        return <p className="text-error text-center mt-40">Cannot retrieve repository</p>;
+    }
+
+    const {
+        id,
+        name,
+        issues,
+        stargazerCount,
+        watchers: { totalCount },
+    } = result.data.repository;
+
+    return (
+        <div className="w-container mx-auto pt-32">
+            <Searchbox />
+            <div>
+                <div className="mt-24 flex justify-between items-center">
+                    <h1 className="text-5xl">{name}</h1>
+                    <span className="text-xl">
+                        {stargazerCount} Stars • {totalCount} Watching
+                    </span>
+                </div>
+                <div>
+                    <div className="flex justify-between mt-12 mb-3">
+                        <h2 className="font-bold text-xl">Open issues</h2>
+                        <Form action="new-issue">
+                            <button type="submit" className="text-lg px-12 py-1 bg-success">
+                                Create issue
+                            </button>
+                        </Form>
+                    </div>
+
+                    {issues.nodes.length === 0 && <p>No issues have been opened</p>}
+                    {issues.nodes.length > 0 && (
+                        <ul>
+                            {issues.nodes.map(({ id, title, createdAt, number, author: { login } }) => {
+                                const createdAtDate = new Date(createdAt);
+                                const daysFromNow = differenceInDays(new Date(), createdAtDate);
+
+                                return (
+                                    <li key={id} className="px-1 py-4 border-t last:border-b">
+                                        <h3 className="mb-1 font-bold text-lg">{title}</h3>
+                                        <span className="text-gray1 text-sm">
+                                            #{number} opened{' '}
+                                            <span title={createdAtDate.toString()}>{daysFromNow} days ago</span> by{' '}
+                                            {login}
+                                        </span>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    )}
+                    <Outlet context={{ repositoryId: id }} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Repository;
